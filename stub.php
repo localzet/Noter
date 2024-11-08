ccaf1f5a083b0f757371f117c0733da4469d7073
Ivan Zorin <creator@localzet.com>
2024-11-08 17:16:13 +0300

    Update stub.php

--- stub.php
+++ stub.php
@@ -23,7 +23,27 @@ define('BASE_PATH', $pharPath
 );
 
 
-$console = new Console();
+$console = new Console([
+    'build' => [
+        'input_dir' => BASE_PATH,
+        'output_dir' => BASE_PATH . DIRECTORY_SEPARATOR . 'build',
+
+        'exclude_pattern' => '#^(?!.*(composer.json|/.github/|/.idea/|/.git/|/.setting/|/runtime/|/vendor-bin/|/build/))(.*)$#',
+        'exclude_files' => [
+            'noter.zconf', 'LICENSE', 'composer.json', 'composer.lock', 'localzet.phar', 'localzet.bin', 'build.php'
+        ],
+
+        'phar_alias' => 'noter',
+        'phar_filename' => 'noter.phar',
+        'phar_stub' => 'stub.php',
+
+        'signature_algorithm' => Phar::SHA256, // Phar::MD5, Phar::SHA1, Phar::SHA256, Phar::SHA512, Phar::OPENSSL.
+        'php_version' => 8.2,
+        'custom_ini' => 'memory_limit = 256M',
+
+        'bin_filename' => 'noter',
+    ],
+]);
 $console->add(new NoteCommand);
 $console->setDefaultCommand('note', true);
 $console->run();
